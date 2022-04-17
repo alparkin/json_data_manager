79aa1a8a0945832e5c6678160f4590c5b4ec734b
Алексей Паркин <a.parkin.personal@gmail.com>
2022-04-17 20:38:13 +0500

    Создание графического интерфейса

--- app/src/main/java/com/examples/jsondatamanager/MainActivity.java
+++ app/src/main/java/com/examples/jsondatamanager/MainActivity.java
@@ -3,6 +3,8 @@ package com.examples.jsondatamanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editTextFirstName = findViewById(R.id.edit_text_first_name);
+        EditText editTextLastName = findViewById(R.id.edit_text_last_name);
+
+        Button readButton = findViewById(R.id.button_read);
+        readButton.setOnClickListener(v->{
+
+        });
+
+        Button saveButton = findViewById(R.id.button_save);
+        saveButton.setOnClickListener(v->{
+
+        });
     }
 }
